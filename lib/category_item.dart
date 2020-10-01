46b27a0e8bb318e4273369e2cbd0834f04846b3e
ernestkoko <ernesteferetin@yahoo.com>
2020-10-01 14:22:09 +0100

    Added a screen that is being navigated to when a category is tapped

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   CategoryItem({this.title, this.color});
@@ -6,25 +7,37 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (_) => CategoryMealsScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15.0),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          //set the direction
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    //InkWell is a gesture detector that sends a ripple effect.. and you can
+    //configure the anim
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15.0),
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
         ),
-        borderRadius: BorderRadius.circular(15.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            //set the direction
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
       ),
     );
   }
